1da948b41b4656d1fab1b04192e4c83a2a7f457f
Rodney Rehm <mail+github@rodneyrehm.de>
2014-10-23 09:22:27 +0200

    simplify findIndex code

--- src/array/findIndex.js
+++ src/array/findIndex.js
@@ -3,15 +3,20 @@ define(function defineArrayFindIndex(require) {
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
   function findIndex(list, callback, context) {
+    var index = -1;
+
     if (context) {
       callback = callback.bind(context);
     }
 
-    var position = list.reduce(function(previous, element, index, array) {
-      return previous === null && callback(element, index, array) ? index : previous;
-    }, null);
+    for (var i=0; i < list.length; i++) {
+      if (callback(list[i], i, list)) {
+        index = 1;
+        break;
+      }
+    }
 
-    return position === null ? -1 : position;
+    return index;
   }
 
   function findIndexES6(list, callback, context) {
