ea71614dfcbf512948143c3430c2e2e32a17dbbe
Rodney Rehm <mail+github@rodneyrehm.de>
2014-10-27 10:27:56 +0100

    adding some more notes on results

--- demo/focusable/data/notes.js
+++ demo/focusable/data/notes.js
@@ -7,14 +7,20 @@ define(function defineDemoFocusableNotes(require) {
 
     "table.error-by-visibility": 'TODO: investigate this behavior',
     "table tr td.error-by-visibility": 'TODO: investigate this behavior',
-    "svg": "TODO: investigate svg focus",
+
+    "svg": '&lt;svg&gt; could be considered embedded content (like &lt;object&gt;) in which case it should be focusable',
+    "svg a[xlink|href]": '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=369507">Bug 369507: default visual indication of focus</a>',
 
     "fieldset[tabindex=0][disabled]": 'should not be focusable as per <a href="http://www.w3.org/TR/html5/disabled-elements.html#concept-element-disabled">disabled elements</a>',
     "link[itemprop][href]": 'naturally focusable according to <a href="http://www.w3.org/TR/html5/editing.html#sequential-focus-navigation-and-the-tabindex-attribute">HTML5 tabindex</a>',
 
     "a > img[ismap]": 'see <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-ismap">&lt;img ismap&gt;</a>, focus should be the link-parent',
-    "area[href].upper" : 'tabbable but not focusable in Firefox. Also should fire twice, does not!',
-    "area[href].lower" : 'tabbable but not focusable in Firefox. Also should fire twice, does not!',
+    "area[href].upper" : 'possibly related bugs: <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=672039">672039</a>, <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=866373">866373</a>, <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=135083">135083</a>',
+    "area[href].lower" : 'possibly related bugs: <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=672039">672039</a>, <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=866373">866373</a>, <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=135083">135083</a>',
+
+    "via(img[usemap].first): area[href].upper": 'upon focusing an image with associated &lt;map&gt;, IE will focus the first &lt;area&gt; of that map.',
+    "via(img[usemap].second): area[href].upper": 'upon focusing an image with associated &lt;map&gt;, IE will focus the first &lt;area&gt; of that map.',
+    "via(label[for=\"label-target\"]): input[type=text]": 'upon focusing <label for> the element identified by the for attribute gets focused',
 
     // invalid [tabindex]
     "[tabindex=\"\"]": 'invalid according to <a href="http://www.w3.org/TR/html5/infrastructure.html#rules-for-parsing-integers">rules for parsing integers</a> required by <a href="http://www.w3.org/TR/html5/editing.html#sequential-focus-navigation-and-the-tabindex-attribute">HTML5 tabindex</a>',
