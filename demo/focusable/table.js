c8575d34f6324a5aa757c0e6fb0650e95897ca17
Rodney Rehm <mail+github@rodneyrehm.de>
2014-10-27 12:34:07 +0100

    adding JS libraries to support table

--- demo/focusable/table.js
+++ demo/focusable/table.js
@@ -22,11 +22,14 @@ require([
   var $table = $('#focusable-table');
   var $tbody = $table.find('.items')
   var $versions = $table.find('.versions');
+  // prepare template for actual data
   var $row = $versions.clone();
   $row.children().replaceWith(function(index) {
-    return index ? $('<td></td>').attr('data-column', $(this).attr('data-column')) : this;
+    var $this = $(this);
+    return index ? $('<td></td>').prop('className', $this.prop('className')).attr('data-column', $this.attr('data-column')) : this;
   });
 
+  // fill in proper versions for each browser column
   $versions.children().each(function() {
     var $cell = $(this);
     var key = $cell.attr('data-column');
@@ -40,8 +43,9 @@ require([
       .attr('title', data[key].platform.ua);
   });
 
+  // add rows of actual data
   selectors.sort().forEach(function(selector) {
-    var $_row = $row.clone().attr('data-row', selector);
+    var $_row = $row.clone().attr('data-selector', selector);
     var $cells = $_row.children('td').not('[data-column="notes"]');
     var expected = data.expected.focusable.indexOf(selector) !== -1;
 
@@ -58,6 +62,47 @@ require([
     });
 
     $_row.appendTo($tbody);
+  });
+
+  // prepare second table that shows support of libraries in given browser
+  var $scriptTable = $('#script-focusable-table');
+  $scriptTable.empty().append($table.children().clone());
+  $scriptTable.find('[data-column="notes"]').remove();
+  $scriptTable.find('thead td, thead th').not('.meta').attr('colspan', function() {
+    return ($(this).attr('colspan') || 1) * 3;
+  }).css('border', '3px solid black');
+
+  var $subline = $scriptTable.find('thead tr').last().clone().appendTo($scriptTable.children('thead'));
+  $subline.find('.meta').remove();
+  $subline.prepend('<td colspan="2">');
+  $subline.find('th').replaceWith(function() {
+    return $('<th style="border-left: 3px solid black">Browser</th><th>a11y.js</th><th style="border-right: 3px solid black">jQuery UI</th>');
+  });
+
+  $scriptTable.find('tbody tr').each(function() {
+    var $_row = $(this);
+    var selector = $_row.attr('data-selector');
+    $_row.children('td').not('.meta').each(function() {
+      var $browserCell = $(this);
+      var browser = $browserCell.attr('data-column');
+      var browserSupported = $browserCell.attr('data-supported') === 'yes';
+      var a11ySupported = data[browser].a11y.focusable.indexOf(selector) !== -1;
+      var jquerySupported = data[browser].jquery.focusable.indexOf(selector) !== -1;
+
+      var $a11yCell = $('<td>?</td>').insertAfter($browserCell)
+        .text(a11ySupported ? 'yes' : 'no')
+        .attr('data-supported', a11ySupported ? 'yes' : 'no')
+        .attr('data-correct', browserSupported === a11ySupported ? 'yes' : 'no');
+
+      var $jqueryCell = $('<td>!</td>').insertAfter($a11yCell)
+        .text(jquerySupported ? 'yes' : 'no')
+        .attr('data-supported', jquerySupported ? 'yes' : 'no')
+        .attr('data-correct', browserSupported === jquerySupported ? 'yes' : 'no');
+
+      // make browser versions distinguishable
+      $browserCell.css('border-left', '3px solid black');
+      $jqueryCell.css('border-right', '3px solid black');
+    })
   });
 
 });
