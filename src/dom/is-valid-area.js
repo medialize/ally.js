37088b3c138dd5abe27af0f4187660492ee89b78
Rodney Rehm <mail+github@rodneyrehm.de>
2014-11-01 21:31:01 +0100

    fixing dom/is-valid-area to exclude broken images in firefox (only)

--- src/dom/is-valid-area.js
+++ src/dom/is-valid-area.js
@@ -4,6 +4,13 @@ define(function defineDomIsValidArea(require) {
   require('CSS.escape');
   var isVisible = require('./is-visible');
   var path = require('./path');
+  var detectFeatureFocus = require('./detect-feature-focus');
+
+  var canFocusBrokenImageMaps = detectFeatureFocus('div', function(element) {
+    element.innerHTML = '<map name="broken-image-map-test"><area href="#void" shape="rect" coords="63,19,144,45"></map>'
+      + '<img usemap="#broken-image-map-test" alt="" src="data:image/png;base64,broken-image-test">';
+      return element.querySelector('area');
+  });
 
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/map
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-usemap
@@ -21,8 +28,18 @@ define(function defineDomIsValidArea(require) {
       return false;
     }
 
+    // HTML5 specifies HTMLMapElement.images to be an HTMLCollection of all
+    // <img> and <object> referencing the <map> element, but no browser implements this
+    //   http://www.w3.org/TR/html5/embedded-content-0.html#the-map-element
+    //   https://developer.mozilla.org/en-US/docs/Web/API/HTMLMapElement
     var img = document.querySelector('img[usemap="#' + CSS.escape(map.name) + '"]');
     if (!img || !isVisible(img)) {
+      return false;
+    }
+
+    // Firefox only allows fully loaded images to reference image maps
+    // https://stereochro.me/ideas/detecting-broken-images-js
+    if (!canFocusBrokenImageMaps && (!img.complete || !img.naturalHeight)) {
       return false;
     }
 
