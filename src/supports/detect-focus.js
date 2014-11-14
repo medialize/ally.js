2c9fb91b5c130e3640a7c8de404cd0d131734317
Rodney Rehm <mail+github@rodneyrehm.de>
2014-11-14 15:27:57 +0100

    fixing focus detection tests so they are hidden from screen readers

--- src/supports/detect-focus.js
+++ src/supports/detect-focus.js
@@ -8,12 +8,19 @@ define(function defineSupportsDetectFocus(require) {
   //  {function} callback(element) to manipulate element prior to focus-test.
   //             Can return DOMElement to define focus target (default: element)
   function detectFocus(nodeName, callback) {
+    // wrap tests in an element hidden from screen readers to prevent them
+    // from announcing focus, which can be quite irritating to the user
+    var wrapper = document.createElement('div');
+    wrapper.setAttribute('aria-live', 'off');
+    wrapper.setAttribute('aria-busy', 'true');
+    wrapper.setAttribute('aria-hidden', 'true');
     // create dummy element to test focusability of
     var element = typeof nodeName === 'string' ? document.createElement(nodeName) : nodeName();
     // allow callback to further specify dummy element
     var focus = callback && callback(element) || element;
     // element needs to be part of the DOM to be focusable
-    document.body.appendChild(element);
+    wrapper.appendChild(element);
+    document.body.appendChild(wrapper);
     // remember what had focus to restore after test
     var previousActiveElement = document.activeElement;
     // test if the element with invalid tabindex can be focused
