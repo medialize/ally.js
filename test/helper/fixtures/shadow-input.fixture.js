246cf8ecb106711278c8f8cf9f8c6658bb5a6ad1
Rodney Rehm <mail+github@rodneyrehm.de>
2015-08-06 19:01:32 +0200

    test(query/focusable): adding shadow dom test

--- test/helper/fixtures/shadow-input.fixture.js
+++ test/helper/fixtures/shadow-input.fixture.js
@@ -1,5 +1,38 @@
 define([], function() {
-  return function(context) {
+
+  function createShadowRoot(fixture) {
+    if (!fixture.shadow) {
+      fixture.shadow = {};
+    }
+
+    if (!fixture.input) {
+      fixture.input = {};
+    }
+
+    var firstShadowHost = document.getElementById('first-shadow-host');
+    var firstShadowRoot = firstShadowHost.createShadowRoot();
+    firstShadowRoot.innerHTML = [
+      '<input id="first-input" type="text" value="first-input">',
+      '<div id="second-shadow-host"></div>',
+      '<div id="third-shadow-host"></div>',
+    ].join('');
+    fixture.input.first = firstShadowRoot.getElementById('first-input');
+    fixture.shadow.first = firstShadowHost;
+
+    var secondShadowHost = firstShadowRoot.getElementById('second-shadow-host');
+    var secondShadowRoot = secondShadowHost.createShadowRoot();
+    secondShadowRoot.innerHTML = '<input id="second-input" type="text" value="second-input">';
+    fixture.input.second = secondShadowRoot.getElementById('second-input');
+    fixture.shadow.second = secondShadowHost;
+
+    var thirdShadowHost = firstShadowRoot.getElementById('third-shadow-host');
+    var thirdShadowRoot = thirdShadowHost.createShadowRoot();
+    thirdShadowRoot.innerHTML = '<input id="third-input" type="text" value="third-input">';
+    fixture.input.third = thirdShadowRoot.getElementById('third-input');
+    fixture.shadow.third = thirdShadowHost;
+  }
+
+  function createFixture(context) {
     var fixture = {
       root: document.createElement('div'),
       shadow: {},
@@ -27,28 +60,12 @@ define([], function() {
       return fixture;
     }
 
-    var firstShadowHost = document.getElementById('first-shadow-host');
-    var firstShadowRoot = firstShadowHost.createShadowRoot();
-    firstShadowRoot.innerHTML = [
-      '<input id="first-input" type="text" value="first-input">',
-      '<div id="second-shadow-host"></div>',
-      '<div id="third-shadow-host"></div>',
-    ].join('');
-    fixture.input.first = firstShadowRoot.getElementById('first-input');
-    fixture.shadow.first = firstShadowHost;
-
-    var secondShadowHost = firstShadowRoot.getElementById('second-shadow-host');
-    var secondShadowRoot = secondShadowHost.createShadowRoot();
-    secondShadowRoot.innerHTML = '<input id="second-input" type="text" value="second-input">';
-    fixture.input.second = secondShadowRoot.getElementById('second-input');
-    fixture.shadow.second = secondShadowHost;
-
-    var thirdShadowHost = firstShadowRoot.getElementById('third-shadow-host');
-    var thirdShadowRoot = thirdShadowHost.createShadowRoot();
-    thirdShadowRoot.innerHTML = '<input id="third-input" type="text" value="third-input">';
-    fixture.input.third = thirdShadowRoot.getElementById('third-input');
-    fixture.shadow.third = thirdShadowHost;
+    createShadowRoot(fixture);
 
     return fixture;
-  };
+  }
+
+  createFixture.createShadowRoot = createShadowRoot;
+
+  return createFixture;
 });
--- test/unit/query.focusable.test.js
+++ test/unit/query.focusable.test.js
@@ -2,10 +2,21 @@ define([
   'intern!object',
   'intern/chai!expect',
   '../helper/fixtures/focusable.fixture',
+  '../helper/fixtures/shadow-input.fixture',
   '../helper/elements-string',
   'ally/supports/focus-invalid-tabindex',
+  'ally/supports/css-shadow-piercing-deep-combinator',
   'ally/query/focusable',
-], function(registerSuite, expect, focusableFixture, elementsString, canFocusInvalidTabindex, queryFocusable) {
+], function(
+  registerSuite,
+  expect,
+  focusableFixture,
+  shadowInputFixture,
+  elementsString,
+  canFocusInvalidTabindex,
+  cssShadowPiercingDeepCombinator,
+  queryFocusable
+) {
 
   registerSuite(function() {
     var fixture;
@@ -49,6 +60,27 @@ define([
           context: '.context',
           includeContext: true,
         });
+
+        expect(elementsString(result)).to.equal(expected);
+      },
+
+      'Shadow DOM': function() {
+        if (document.body.shadowRoot === undefined) {
+          this.skip('Shadow DOM not supported');
+        }
+
+        if (!cssShadowPiercingDeepCombinator) {
+          this.skip('Shadow DOM "shadow-piercing descendant combinator" not supported');
+        }
+
+        var host = document.createElement('div');
+        host.id = 'first-shadow-host';
+        fixture.root.appendChild(host);
+        shadowInputFixture.createShadowRoot(fixture);
+
+        var expected = 'body, #tabindex--1, #tabindex-0, #tabindex-1, #link, #link-tabindex--1, #input, #input-tabindex--1'
+          + ', #first-input, #second-input, #third-input';
+        var result = queryFocusable();
 
         expect(elementsString(result)).to.equal(expected);
       },
