e2a78d6a1cb61e5d6864bb55f343a19561b803bd
Rodney Rehm <mail+github@rodneyrehm.de>
2015-11-20 15:08:22 +0100

    chore(website): pushing website to gh-pages

--- examples/main.js
+++
@@ -1,9 +0,0 @@
-require.config({
-  urlArgs: "bust=" +  (new Date()).getTime(),
-  paths: {
-    ally: '../src',
-    // shims required by ally.js
-    'array.prototype.findindex': '../bower_components/array.prototype.findindex/index',
-    'CSS.escape': '../bower_components/CSS.escape/css.escape',
-  }
-});
--- tests/fix-pointer-focus/fix-pointer-focus.js
+++
@@ -1,9 +0,0 @@
-require.config({
-  paths: {
-    ally: '../../src'
-  }
-});
-
-require(['ally/fix-browser/pointer-focus'], function (fixPointerFocus) {
-  fixPointerFocus(document);
-});
