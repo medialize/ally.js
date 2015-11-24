da535f21ee69c8377201e728108a987c6ef06105
Rodney Rehm <mail+github@rodneyrehm.de>
2015-11-25 00:10:39 +0100

    test(element.prototype.matches): IE9 requires the tested element to be attached to a document - #71

--- test/unit/prototype.element.prototype.matches.test.js
+++ test/unit/prototype.element.prototype.matches.test.js
@@ -1,14 +1,26 @@
 define([
   'intern!object',
   'intern/chai!expect',
+  '../helper/fixtures/custom.fixture',
   '../helper/function-name',
   'ally/prototype/element.prototype.matches',
-], function(registerSuite, expect, getFunctionName) {
+], function(registerSuite, expect, customFixture, getFunctionName) {
 
   registerSuite(function() {
+    var fixture;
 
     return {
       name: 'prototype/element.prototype.matches',
+
+      beforeEach: function() {
+        fixture = customFixture([
+          '<div id="target"></div>',
+        ].join(''));
+      },
+      afterEach: function() {
+        fixture.remove();
+        fixture = null;
+      },
 
       prefixed: function() {
         if (getFunctionName(Element.prototype.matches) === 'matches') {
@@ -27,7 +39,7 @@ define([
           this.skip('Element.prototype.matches is not vendor prefixed');
         }
 
-        var element = document.createElement('div');
+        var element = document.getElementById('target');
         expect(element.matches).to.be.a('function');
         expect(element.matches('div')).to.equal(true);
         expect(element.matches('body')).to.equal(false);
