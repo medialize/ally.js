1ac7a4dabbc996801fb11e1fa69854e42528f1a4
Rodney Rehm <mail+github@rodneyrehm.de>
2015-12-09 00:38:38 +0100

    refactor(query/tabsequence.sort-area): reducing complexity by separating code

--- src/query/tabsequence.sort-area.js
+++ src/query/tabsequence.sort-area.js
@@ -7,46 +7,67 @@ import queryTabbable from './tabbable';
 import mergeInDomOrder from '../util/merge-dom-order';
 import getDocument from '../util/get-document';
 
-export default function(elements, context) {
-  // images - unless they are focusable themselves, likely not
-  // part of the elements list, so we'll have to find them and
-  // sort them into the elements list manually
-  const usemaps = context.querySelectorAll('img[usemap]');
-  const _document = getDocument(context);
+class Maps {
+  constructor(context) {
+    this._document = getDocument(context);
+    this.maps = {};
+  }
 
-  // container for the <area> elements we're going to pull out of the elements list
-  const maps = {};
-  const addMapByName = function(name) {
+  getAreasFor(name) {
+    if (!this.maps[name]) {
+      // the map is not defined within the context, so we
+      // have to go find it elsewhere in the document
+      this.addMapByName(name);
+    }
+
+    return this.maps[name];
+  }
+
+  addMapByName(name) {
     // apparently getElementsByName() also considers id attribute in IE & opera
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByName
-    const map = _document.querySelector('map[name="' + CSS.escape(name) + '"]') || null;
+    const map = this._document.querySelector('map[name="' + CSS.escape(name) + '"]') || null;
     if (!map) {
       // if there is no map, the img[usemap] wasn't doing anything anyway
       return;
     }
 
-    maps[map.name] = queryTabbable({context: map});
-  };
+    this.maps[map.name] = queryTabbable({context: map});
+  }
+
+  extractAreasFromList(elements) {
+    // remove all <area> elements from the elements list,
+    // but put them the map for later retrieval
+    return elements.filter(function(element) {
+      const nodeName = element.nodeName.toLowerCase();
+      if (nodeName !== 'area') {
+        return true;
+      }
+
+      const map = element.parentNode;
+      if (!this.maps[map.name]) {
+        this.maps[map.name] = [];
+      }
+
+      this.maps[map.name].push(element);
+      return false;
+    }, this);
+  }
+}
+
+export default function(elements, context) {
+  // images - unless they are focusable themselves, likely not
+  // part of the elements list, so we'll have to find them and
+  // sort them into the elements list manually
+  const usemaps = context.querySelectorAll('img[usemap]');
+  const maps = new Maps(context);
 
   // remove all <area> elements from the elements list,
   // but put them the map for later retrieval
-  const _elements = elements.filter(function(element) {
-    const nodeName = element.nodeName.toLowerCase();
-    if (nodeName !== 'area') {
-      return true;
-    }
-
-    const map = element.parentNode;
-    if (!maps[map.name]) {
-      maps[map.name] = [];
-    }
-
-    maps[map.name].push(element);
-    return false;
-  });
+  const _elements = maps.extractAreasFromList(elements);
 
   if (!usemaps.length) {
-    // the context does not contain any images so no need
+    // the context does not contain any <area>s so no need
     // to replace anything, just remove any maps
     return _elements;
   }
@@ -56,12 +77,7 @@ export default function(elements, context) {
     elements: usemaps,
     resolveElement: function(image) {
       const name = image.getAttribute('usemap').slice(1);
-      if (!maps[name]) {
-        // the map is not defined within the context, so we
-        // have to go find it elsewhere in the document
-        addMapByName(name);
-      }
-      return maps[name];
+      return maps.getAreasFor(name);
     },
   });
 }
