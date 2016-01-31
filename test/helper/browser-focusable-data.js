d3035f4a7720da0299f2e179012b0188781f2282
Rodney Rehm <mail+github@rodneyrehm.de>
2016-01-31 18:47:43 +0100

    chore(browser/focusable): retain chrome 47 for BrowserStack

--- test/helper/browser-focusable-data.js
+++ test/helper/browser-focusable-data.js
@@ -1,6 +1,7 @@
 define([
   'intern/dojo/text!../../tests/focusable/data/chrome-nightly.json',
   'intern/dojo/text!../../tests/focusable/data/chrome-stable.json',
+  'intern/dojo/text!../../tests/focusable/data/chrome-47.json',
   'intern/dojo/text!../../tests/focusable/data/firefox-nightly.json',
   'intern/dojo/text!../../tests/focusable/data/firefox-stable.json',
   'intern/dojo/text!../../tests/focusable/data/ie-13.json',
