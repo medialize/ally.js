8a6b45b4b6cb36406ae30ebdfa7890a92473f666
Rodney Rehm <mail+github@rodneyrehm.de>
2016-02-02 00:10:56 +0100

    test(intern.events): adding shift+tab test

--- test/functional/intern.events.test.js
+++ test/functional/intern.events.test.js
@@ -41,6 +41,40 @@ define([
           DOMFocusIn:second,
           keyup:second,
         ]
+        pressKeys(SHIFT TAB) input [
+          keydown:second,
+          keydown:second,
+          blur:second,
+          focusout:second,
+          DOMFocusOut:second,
+          focus:first,
+          focusin:first,
+          DOMFocusIn:first,
+          keyup:first,
+          keyup:first,
+        ]
+        pressKeys(TAB) [
+          keydown:first:9,
+          blur:first,
+          focusout:first,
+          DOMFocusOut:first,
+          focus:second,
+          focusin:second,
+          DOMFocusIn:second,
+          keyup:second:9,
+        ]
+        pressKeys(SHIFT TAB) [
+          keydown:second:16:shift,
+          keydown:second:9:shift,
+          blur:second,
+          focusout:second,
+          DOMFocusOut:second,
+          focus:first,
+          focusin:first,
+          DOMFocusIn:first,
+          keyup:first:9:shift,
+          keyup:first:16,
+        ]
 
       IE 11.0 (SauceLabs)
         click input [
@@ -72,6 +106,35 @@ define([
           blur:first,
           focus:second,
           keyup:second,
+        ]
+        pressKeys(SHIFT TAB) input [
+          keydown:second,
+          keydown:second,
+          focusout:second,
+          focusin:first,
+          blur:second,
+          focus:first,
+          keyup:first,
+          keyup:first,
+        ]
+        pressKeys(TAB) [
+          keydown:first:9,
+          focusout:first,
+          focusin:second,
+          blur:first,
+          focus:second,
+          keyup:second:9,
+        ]
+        // NOTE: event.shiftKey is not set
+        pressKeys(SHIFT TAB) (
+          keydown:second:16,
+          keydown:second:9,
+          focusout:second,
+          focusin:first,
+          blur:second,
+          focus:first,
+          keyup:first:9,
+          keyup:first:16,
         ]
     */
 
@@ -136,6 +199,64 @@ define([
           .findById('first')
             .pressKeys(keys.TAB)
             .end()
+
+          .execute('var _events = window.events.slice(0); window.events.length = 0; return _events;')
+          .then(function(events) {
+            this.skip(events.join(', '));
+          }.bind(this));
+      },
+
+      'pressKeys(SHIFT TAB) input': function() {
+        this.timeout = timeout;
+        return this.remote
+          .findById('second')
+            .click()
+            .end()
+
+          .sleep(500)
+          .execute('window.events.length = 0; return null;')
+
+          .findById('first')
+            .pressKeys([keys.SHIFT, keys.TAB])
+            .pressKeys(keys.NULL)
+            .end()
+
+          .execute('var _events = window.events.slice(0); window.events.length = 0; return _events;')
+          .then(function(events) {
+            this.skip(events.join(', '));
+          }.bind(this));
+      },
+
+      'pressKeys(TAB)': function() {
+        this.timeout = timeout;
+        return this.remote
+          .findById('first')
+            .click()
+            .end()
+
+          .sleep(500)
+          .execute('window.events.length = 0; return null;')
+
+          .pressKeys(keys.TAB)
+
+          .execute('var _events = window.events.slice(0); window.events.length = 0; return _events;')
+          .then(function(events) {
+            this.skip(events.join(', '));
+          }.bind(this));
+      },
+
+      'pressKeys(SHIFT TAB)': function() {
+        this.timeout = timeout;
+        return this.remote
+          .findById('second')
+            .click()
+            .end()
+
+          .sleep(500)
+          .execute('window.events.length = 0; return null;')
+
+          .pressKeys([keys.SHIFT, keys.TAB])
+          .pressKeys(keys.NULL)
 
           .execute('var _events = window.events.slice(0); window.events.length = 0; return _events;')
           .then(function(events) {
