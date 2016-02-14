f046093dfb48b0fa08c32521165e6187c91d5f51
Rodney Rehm <mail+github@rodneyrehm.de>
2016-02-14 16:00:15 +0100

    test(fix/pointer-focus-children): increasing coverage

--- test/functional/fix.pointer-focus-children.test.js
+++ test/functional/fix.pointer-focus-children.test.js
@@ -66,6 +66,36 @@ define([
       'div[tabindex="-1"] > div{flexbox} > span': makeFocusClickTest('nested-', true),
       'input + label': makeFocusClickTest('redirect-', false),
       'input + label{flexbox} > span': makeFocusClickTest('redirect-flexed-', false),
+
+      'div[tabindex="-1"]': function() {
+        this.timeout = timeout;
+        return this.remote
+
+          // This fix is only relevant to IE10 (Trident/6) and IE11 (Trident/7)
+          .then(pollUntil('return window.platform'))
+          .then(function(platform) {
+            if (!platform.is.IE10 && !platform.is.IE11) {
+              this.skip('irrelevant to current browser');
+            }
+          }.bind(this))
+
+          // I have no clue why, but IE11 needs this for
+          // the next click to actually focus something.
+          .findByCssSelector('body')
+            .click()
+            .end()
+          .sleep(500)
+
+          // make sure we're not failing with the fix
+          .findById('natural-fixed-source')
+            .click()
+            .end()
+          .sleep(500)
+          .execute('return document.activeElement.id || document.activeElement.nodeName')
+          .then(function(activeElementId) {
+            expect(activeElementId).to.equal('natural-fixed-source');
+          });
+      },
     };
   });
 });
