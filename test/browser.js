3b569335acec4d2077951eecfab53d5cdb63aab1
Rodney Rehm <mail+github@rodneyrehm.de>
2016-03-26 11:40:42 +0100

    chore(test): adding test:server to run intern in proxy-only mode

--- test/browser.js
+++ test/browser.js
@@ -2,6 +2,10 @@ define([
   './intern',
 ], function(config) {
 
+  // make sure to start the proxy, but not run the tests when
+  // this config is used with the intern-runner CLI
+  config.proxyOnly = true;
+
   delete config.reporters;
 
   return config;
